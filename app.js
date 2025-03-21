e446f0721d1f17b661e03c9a3ad994993c2f02de
bafredo <muyomafredrick@gmail.com>
2025-03-21 11:18:06 +0300

    Form validation

--- app.js
+++ app.js
@@ -1,7 +1,46 @@
 const login=document.getElementById("btn-login")
-login.addEventListener("click",)
+login.addEventListener("click",()=>{
+    mine()
+})
+
+var authorised = false
+
+var screen = document.getElementById("mainContainer")
+
+
+function isValidEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+let mailerror = document.getElementById('mailerror')
+
+
+
+let email = document.getElementById('email')
+let password = document.getElementById('password')
+let error = document.getElementById('error')
+let perror = document.getElementById('ptext')
+function handleForm() {
+    let text =
+    (email.value == "" || password.value == "")
+        ? "Please fill all of the fields" : ""
+    let merror = (isValidEmail(email.value))? "" : "invalid email"
+  
+    error.innerText = text
+    perror.innerText = (password.value.length < 6)? "must be at least 7 chatacters" : ""
+    mailerror.innerText = merror
+
+    if(isValidEmail(email.value) && password.value.length > 6){
+            mailerror.innerText = ""
+            perror.innerText = ""
+            alert("Login succesful")
+            mine()
+    }
+  }
 
 
 function mine(){
-    
+    screen.style.display = authorised? "flex" : "none"
+    authorised = !authorised
 }
